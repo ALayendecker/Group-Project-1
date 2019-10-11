522549497cfc1e34385fe018af0322d1312ad371
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-10-11 08:49:42 -0500

    changed 'styles.js' to reflect the actual css style sheet names, updated style sheets, updated index. final commit.Tested for functionality

--- assets/javascript/stylechanger.js
+++ assets/javascript/stylechanger.js
@@ -2,24 +2,24 @@ $("#creative").click(function() {
   $(".style-sheets").attr("href", "assets/css/creative.css");
 });
 $("#bummed").click(function() {
-  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+  $(".style-sheets").attr("href", "assets/css/bummed.css");
 });
 $("#chill").click(function() {
-  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+  $(".style-sheets").attr("href", "assets/css/chill.css");
 });
 $("#party").click(function() {
-  $(".style-sheets").attr("href", "assets/css/mad.css");
+  $(".style-sheets").attr("href", "assets/css/party.css");
 });
 $("#working").click(function() {
   $(".style-sheets").attr("href", "assets/css/motivated.css");
 });
 $("#feeling-good").click(function() {
-  $(".style-sheets").attr("href", "assets/css/styles.css");
+  $(".style-sheets").attr("href", "assets/css/happy.css");
 });
 $("#romantic").click(function() {
-  $(".style-sheets").attr("href", "assets/css/mad.css");
+  $(".style-sheets").attr("href", "assets/css/romantic.css");
 });
 $("#lonely").click(function() {
-  $(".style-sheets").attr("href", "assets/css/sleepy.css");
+  $(".style-sheets").attr("href", "assets/css/lonely.css");
 });
 console.log("hello world");
